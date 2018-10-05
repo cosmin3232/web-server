c947862de6e30bc34641b3a9b3fbeda451480a39
Hutanu Cosmin <corbotiuc33@gmail.com>
2018-10-06 02:42:53 +0300

    changes

---
+++ middleware.js
@@ -0,0 +1,14 @@
+
+var middleware = {
+	requireAuthentification: function(req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next) {
+		var date = new Date().toString();
+		console.log(date + ' Request: ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+module.exports = middleware;
